0eee8a74dafe6d9102fc0f16c1ff354777f245a5
Kovács Anna <50516891+kovacsanna77@users.noreply.github.com>
2024-05-13 14:33:33 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1cwBDCCZhtgjcOkeNQSqb-LoDSwDDOo2b
 """
 
-!pip install streamlit
+#!pip install streamlit
 
 import streamlit as st
 import pickle
